c989bf17dc85d754cc930807f51be5ab92436772
sarvadandge29 <sarvadandge@gmail.com>
2024-12-18 14:44:25 +0530

    `Update CustomButton component to accept containerStyle prop`

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { TouchableOpacity, Text, SafeAreaView } from "react-native";
 
-const CustomButton = ({ title, onPress, style, textStyle }) => (
-    <SafeAreaView className="w-full">
+const CustomButton = ({ title, onPress, style, textStyle, containerStyle }) => (
+    <SafeAreaView className={`w-full ${containerStyle}`}>
         <TouchableOpacity
             className={`rounded-full min-h-[62px] justify-center items-center ${style}`}
             activeOpacity={0.7}
---
+++ constants/icons.js
@@ -0,0 +1,6 @@
+const icons = {
+    eye : require("../assets/icons/eye.png"),
+    eyeHide : require("../assets/icons/eyehide.png")
+}
+
+export default icons;
